31ca47cb596d8ffe92ad847135b34d8cb3a22f01
Vinson <vinson7768@gmail.com>
2023-01-17 13:46:27 +0700

    Added user checking segmen to see if user exist in database

--- production/cms/user/register.php
+++ production/cms/user/register.php
@@ -12,21 +12,31 @@
             $res['success'] = 0;
             $res['message'] = 'Mohon masukkan angka atau huruf!';
         } else {
-            $password = password_hash($password, PASSWORD_DEFAULT);
-            $otoritas = implode(' ; ', $_POST['otoritas']);
-            $insert = "INSERT INTO user_account (nama, password, otoritas, status, nama_login) VALUES (?, ?, '$otoritas', ?, ?); ";
-            $stmt = mysqli_prepare($koneksi, $insert);
-            mysqli_stmt_bind_param($stmt, 'ssss', $nama, $password, $aktif, $login);
-            mysqli_stmt_execute($stmt);
-            if(mysqli_stmt_affected_rows($stmt) > 0) {
-                $res['success'] = 1;
-                $res['message'] = 'User baru berhasil dimasukkan!';
-            } else {
+            $check = "SELECT * FROM user_account where nama_login = ?";
+            $statement = mysqli_prepare($koneksi, $check);
+            mysqli_stmt_bind_param($statement, 's', $login);
+            mysqli_stmt_execute($statement);
+            mysqli_stmt_store_result($statement);
+            $rows = mysqli_stmt_num_rows($statement);
+            if($rows > 0) {
                 $res['success'] = 0;
-                $res['message'] = 'User gagal dimasukkan! Periksa koneksi anda!';
-            }
-        }
-    } else {
+                $res['message'] = 'Username sudah terdaftar di database! Mohon masukkan username lain!';
+            } else {
+                $password = password_hash($password, PASSWORD_DEFAULT);
+                $otoritas = implode(' ; ', $_POST['otoritas']);
+                $insert = "INSERT INTO user_account (nama, password, otoritas, status, nama_login) VALUES (?, ?, ?, ?, ?); ";
+                $stmt = mysqli_prepare($koneksi, $insert);
+                mysqli_stmt_bind_param($stmt, 'sssss', $nama, $password, $otoritas, $aktif, $login);
+                mysqli_stmt_execute($stmt);
+                if(mysqli_stmt_affected_rows($stmt) > 0) {
+                    $res['success'] = 1;
+                    $res['message'] = 'User baru berhasil dimasukkan!';
+                } else {
+                    $res['success'] = 0;
+                    $res['message'] = 'User gagal dimasukkan! Periksa koneksi anda!';
+                }
+            } 
+        }} else {
         $res['success'] = 0;
         $res['message'] = 'Mohon pastikan nama, nama login, dan password sudah terisi!';
     }    
