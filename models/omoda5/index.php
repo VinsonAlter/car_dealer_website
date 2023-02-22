797849e8bc49222f1c97f841c33a048ac36e9387
Vinson <vinson7768@gmail.com>
2023-02-22 14:27:49 +0700

    Finish omoda brochure part

--- models/omoda5/index.php
+++ models/omoda5/index.php
@@ -75,9 +75,31 @@
                 width: 100%;
             }
 
-            .tiggo7-text-bottom {
-                top: 35%;
-                font-size: 3rem !important;
+            .omoda-brochure {
+                background: url('assets/img/omoda-brochure.png') no-repeat center center;
+                object-fit:cover;
+                width: 100%;
+                height: 500px;
+            }
+
+            .omoda-brochure-text {
+                top: 12.5%;
+            }
+
+            .fs64-scaled {
+                font-size: 52px;
+            }
+
+            .lh-1half {
+                line-height: 2;
+            }
+
+            .btn-red {
+                background-color: #C00B20;
+            }
+
+            .btn-red:hover {
+                filter: brightness(90%);
             }
 
             .border-rounded {
@@ -104,11 +126,6 @@
                 color: #fff;
             }
 
-            #tiggo7-iframe {
-                width: 100%;
-                height: 800px;
-            }
-
             @media (max-width: 768px) {
                 .tiggo7-text-bottom {
                     top: 30%;
@@ -122,10 +139,10 @@
                 .button-padding h5 {
                     font-size: calc(1rem + .3vw);
                 }
-
-                #tiggo7-iframe {
-                    height: 350px;
-                }  
+                
+                .fs64-scaled {
+                    font-size: 24px;
+                }
             } 
 
         </style>
@@ -155,18 +172,69 @@
             <img class="w-100 lazyload" data-src="assets/img/omoda-cabin.jpg" alt="Futuristic Cabin Design">
             <img class="w-100 lazyload" data-src="assets/img/omoda-ambient-light.jpg" alt="Multicolor Ambient Light">
             <img class="w-100 lazyload" data-src="assets/img/omoda-super-large-screen.jpg" alt="Multicolor Ambient Light">
+            <!-- Omoda 5 Features -->
             <div class="p-4 d-block">
                 <h1 class="omoda-font">Features</h1> 
             </div>
             <img class="w-100 lazyload" data-src="assets/img/omoda-wireless-charger.jpg" alt="Wireless Charger with Phone Reminder">
             <img class="w-100 lazyload" data-src="assets/img/omoda-premium-audio.jpg" alt="Sony Premium Audio Sound">
             <img class="w-100 lazyload" data-src="assets/img/omoda-door-mirror-heating.jpg" alt="Door Mirror Heating">
+            <!-- Omoda 5 Specification -->
             <div class="p-4 d-block">
                 <h1 class="omoda-font">Specification</h1> 
             </div>
             <img class="w-100 lazyload" data-src="assets/img/omoda-spec.jpg" alt="Omoda 5 Specification">
             <img class="w-100 lazyload" data-src="assets/img/omoda-suv.jpg" alt="Omoda 5 Dynamic and Cross-over SUV">
             <img class="w-100 lazyload" data-src="assets/img/omoda-engine-tgdi.jpg" alt="Omoda 5 1.5 TGDI Engine">
+            <!-- Omoda 5 Brochure Section -->
+            <div class="omoda-brochure">
+            <div class="container-fluid pl-0">
+                <div class="d-flex work-font text-white cp">
+                    <div class="col-3 col-md-4 mr-2">
+                        <!-- Temporarily still used the google form test drive -->
+                        <div class = "text-center tiggo-nav" onclick="window.open('https://forms.gle/csGRdsmUEcbmxdxF8', '_blank')">
+                            <!-- In future, this test drive form will be used, below is just the template -->
+                            <!-- <div class = "text-center tiggo-nav" onclick="window.open('<?=BASE_URL?>test-drive/', '_blank')">     -->
+                            <i class="fa-solid fa-car-on"></i>
+                            <p class="p-0 m-0 fs-bold-600">
+                                Test Drive
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-5 col-md-4 mr-2">
+                        <div class = "text-center tiggo-nav" onclick="window.open('<?=BASE_URL?>price-list/assets/price-list/tiggo8pro-pricelist-medan.webp', '_blank')">
+                            <i class="fas fa-book-reader"></i> 
+                            <p class="p-0 m-0 fs-bold-600">
+                                Unduh Price List
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-4 col-md-4">
+                        <div class = "text-center tiggo-nav" onclick="window.open('https://forms.gle/nh9R9RwHzGug2MeF9', '_blank')">
+                            <i class="fas fa-comments"></i>                 
+                            <p class="p-0 m-0 fs-bold-600">
+                                Kontak Kami
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <div class = "px-5 d-flex flex-column relative omoda-font omoda-brochure-text">
+                    <p class="relative text-white mb-2 fw-bold fs64-scaled lh-1half">Download Brosur</p>
+                    <p class="relative text-white mb-3 fw-bold fs64-scaled lh-1half">Omoda 5 Sekarang Juga</p>
+                    <a href="javascript:void(0)">
+                        <button class="btn btn-lg fw-bold btn-red text-white">
+                            Download Brosur
+                        </button>
+                    </a>
+                    <!-- <div class="d-flex gap-row mb-5 centered">
+                        <a class="col-12 my-3 button-padding border-rounded bg-light
+                                models-font btn" href="../../price-list/assets/brosur/Tiggo8Pro-brosur.pdf" target="_blank">
+                            <h5 class="text-center">Download Brosur</h5>
+                        </a>                         
+                    </div> -->
+                </div>
+            </div>
         </div>
 
         <footer class="relative footer-border mt-up-8 px-3 py-4">
